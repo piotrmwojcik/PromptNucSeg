99aa54c1898f07b21976b49ed6daffe91eb2642d
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-03-09 20:03:37 +0100

    Added ViTDeT

--- prompter/models/dpa_p2pnet.py
+++ prompter/models/dpa_p2pnet.py
@@ -197,7 +197,7 @@ def build_model(cfg):
     checkpoint_model = checkpoint['model']
     interpolate_pos_embed(model.backbone.backbone, checkpoint_model)
 
-    msg = model.backbone.backbone.load_state_dict(checkpoint_model, strict=False)
+    #msg = model.backbone.backbone.load_state_dict(checkpoint_model, strict=False)
     print('Loading backbone for prompter')
     print(msg)
 

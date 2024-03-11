ed595627fc6e63940870199d15eff4c8ccfad0b3
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-03-11 14:53:56 +0100

    512

--- prompter/models/dpa_p2pnet.py
+++ prompter/models/dpa_p2pnet.py
@@ -152,8 +152,7 @@ class DPAP2PNet(nn.Module):
         # DPP
         grid = (2.0 * proposals / self.strides[0] / feat_sizes[0] - 1.0)
 
-        roi_features = F.grid_sample(feats[2], grid, mode='bilinear', align_corners=True)
-        #roi_features2 = F.grid_sample(x, grid, mode='bilinear', align_corners=True)
+        roi_features = F.grid_sample(feats[0], grid, mode='bilinear', align_corners=True)
         deltas2deform = self.deform_layer(roi_features.permute(0, 2, 3, 1))
         deformed_proposals = proposals + deltas2deform
 

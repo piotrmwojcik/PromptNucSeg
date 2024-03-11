a663549a84091351e0b7ec8c827226a582b14e97
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-03-11 13:19:15 +0100

    512

--- prompter/models/dpa_p2pnet.py
+++ prompter/models/dpa_p2pnet.py
@@ -169,8 +169,9 @@ class DPAP2PNet(nn.Module):
         for i in range(self.num_levels):
             grid = (2.0 * deformed_proposals / self.strides[i] / feat_sizes[i] - 1.0)
             print('!!!')
-            print(grid[0])
-            print()
+            if i == 1:
+                print(grid[0])
+                print()
             #grid = (2.0 * deformed_proposals / 256) - 1.0 - 1.0
             roi_features.append(F.grid_sample(feats[i], grid, mode='bilinear', align_corners=True))
         roi_features = torch.cat(roi_features, 1)

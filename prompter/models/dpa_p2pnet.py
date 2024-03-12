5fd72bf96976247566ed730d71833ef5398f7b3d
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-03-12 17:18:09 +0100

    test

--- prompter/models/dpa_p2pnet.py
+++ prompter/models/dpa_p2pnet.py
@@ -138,7 +138,7 @@ class DPAP2PNet(nn.Module):
         self.conv = nn.Conv2d(hidden_dim * num_levels, hidden_dim, kernel_size=3, padding=1)
 
         self.mask_head = nn.Sequential(
-            nn.Conv2d(hidden_dim, hidden_dim, kernel_size=3, padding=1),
+            nn.Conv2d(256, hidden_dim, kernel_size=3, padding=1),
             nn.SyncBatchNorm(hidden_dim),
             nn.ReLU(inplace=True),
             nn.Conv2d(hidden_dim, 1, kernel_size=1, padding=1)

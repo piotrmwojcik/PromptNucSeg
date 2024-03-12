7b449741b6eb7b4002d1090972b95106d7881f07
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-03-12 17:07:16 +0100

    test

--- prompter/models/dpa_p2pnet.py
+++ prompter/models/dpa_p2pnet.py
@@ -130,7 +130,7 @@ class DPAP2PNet(nn.Module):
         self.with_mask = with_mask
         self.strides = [2 ** (i + 2) for i in range(self.num_levels)]
 
-        self.deform_layer = MLP(768, hidden_dim, 2, 8, drop=dropout)
+        self.deform_layer = MLP(768, 256, 2, 8, drop=dropout)
 
         #self.reg_head = MLP(hidden_dim, hidden_dim, 2, 2, drop=dropout)
         self.cls_head = MLP(hidden_dim, hidden_dim, 2, num_classes + 1, drop=dropout)

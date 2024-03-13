3b2aec5a6c935ad5a45631860b9eb0e6b728e215
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-03-13 21:40:47 +0100

    test

--- prompter/criterion.py
+++ prompter/criterion.py
@@ -52,8 +52,7 @@ class Criterion(nn.Module):
         indices = points.to(torch.int64)
         linear_indices = indices.flatten(0, 1)
 
-        gathered_values = type_map[linear_indices.int()[:, 1], linear_indices.int()[:, 0]]
-        target_classes = gathered_values.view(bs, 1024)
+        target_classes = type_map[linear_indices.int()[:, 1], linear_indices.int()[:, 0]]
 
         loss_cls = F.cross_entropy(src_logits.transpose(1, 2), target_classes.long(), self.class_weight)
         loss_dict = {'loss_cls': loss_cls}

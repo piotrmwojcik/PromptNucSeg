29c3223e2e021edcf336d60d82ab921c957a695c
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-03-13 18:42:27 +0100

    test

--- prompter/dataset.py
+++ prompter/dataset.py
@@ -71,7 +71,7 @@ class DataFolder(Dataset):
                         'type_map']
 
         mask = (mask > 0).astype(float)
-        values.append(mask)
+        #values.append(mask)
         unique_values = np.unique(type_map)
 
         if not len(unique_values[1:]):

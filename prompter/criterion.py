caf8ac924d765346ff2234c6be6227cdeecf06fc
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-03-13 20:44:41 +0100

    test

--- prompter/criterion.py
+++ prompter/criterion.py
@@ -43,7 +43,7 @@ class Criterion(nn.Module):
         #target_classes[idx] = target_classes_o
 
         points = outputs['pred_coords']
-        type_map = targets['type_map']
+        type_map = targets['gt_type_map']
         target_classes = type_map[points.astype(int)[:, 1], points.astype(int)[:, 0]]
 
         loss_cls = F.cross_entropy(src_logits.transpose(1, 2), target_classes, self.class_weight)

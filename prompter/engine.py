a4886592934beefa28cb2fc3444ecb86586772fb
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-03-15 12:08:09 +0100

    test

--- prompter/engine.py
+++ prompter/engine.py
@@ -55,7 +55,7 @@ def train_one_epoch(
         for idx in range(10):
             image = images[idx]
             pd_points = outputs['pred_coords'].clone()[idx]
-            #pd_points = pd_points.detach().cpu().numpy()
+            pd_points = pd_points.detach().cpu().numpy()
             scores = outputs['pred_logits'][0].softmax(-1).detach().cpu().numpy()
             import numpy as np
             classes = np.argmax(scores, axis=-1)

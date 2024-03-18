accf3f995d60b4053fff72da8b7e2655dd7b054d
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-03-18 19:05:43 +0100

    test

--- prompter/criterion.py
+++ prompter/criterion.py
@@ -73,7 +73,7 @@ class Criterion(nn.Module):
         loss_cls2 = F.cross_entropy(src_logits.transpose(1, 2), target_classes.to(torch.long), self.class_weight_all)
         #print(loss_cls1, 0.05 * loss_cls2)
 
-        loss_cls = loss_cls1 + 0.3 * loss_cls2
+        loss_cls = loss_cls1 + 0.05 * loss_cls2
         loss_dict = {'loss_cls': loss_cls}
 
         return loss_dict

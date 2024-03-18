56387c3d0cebc4f87fd05efba9f35c099a3d7f7b
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-03-18 22:29:42 +0100

    test

--- prompter/criterion.py
+++ prompter/criterion.py
@@ -78,7 +78,7 @@ class Criterion(nn.Module):
         #    print(f"Value {value}: {count} occurrences")
         #print()
 
-        loss_cls2 = F.cross_entropy(src_logits.transpose(1, 2), target_classes.to(torch.long), self.class_weight_all)
+        loss_cls2 = F.cross_entropy(src_logits, target_classes.to(torch.long), self.class_weight_all)
 
         loss_cls = loss_cls1 + 0.025 * loss_cls2
         loss_dict = {'loss_cls': loss_cls}

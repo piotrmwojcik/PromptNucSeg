7b3a7243149f576acadf5ff7cbd24f4f080db5e3
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-05-14 17:54:15 +0200

    rollback

--- segmentor/main.py
+++ segmentor/main.py
@@ -266,7 +266,7 @@ def train_on_epoch(
             images=images,
             prompt_labels=prompt_labels,
             cell_nums=cell_nums,
-            prompt_boxes=prompt_points,
+            prompt_points=prompt_points,
         )
 
         loss_dict = criterion(

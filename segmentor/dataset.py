bfbb5668a507d57f96d06f1b01e8eaf52f49e39d
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-05-19 21:17:30 +0200

    rollback

--- segmentor/dataset.py
+++ segmentor/dataset.py
@@ -47,6 +47,15 @@ class DataFolder(Dataset):
     def __len__(self):
         return len(self.files)
 
+    def _expand_box(self, box):
+        if (box[2] - box[0]) < 1.0:
+            box[0] = max(int(box[0]) - 2, 0)
+            box[2] = min(int(box[2]) + 2, 255)
+        if (box[3] - box[1]) < 1.0:
+            box[1] = max(int(box[1]) - 2, 0)
+            box[3] = min(int(box[3]) + 2, 255)
+        return box
+
     def __getitem__(self, idx):
         img_path = self.files[idx]
 
@@ -102,14 +111,8 @@ class DataFolder(Dataset):
                     torch.argwhere(mask_single_cell)
                 )[None, [1, 0]]
 
-                if (box[2] - box[0]) < 1.0:
-                    box[0] = max(int(box[0]) - 2, 0)
-                    box[2] = min(int(box[2]) + 2, 255)
-                if (box[3] - box[1]) < 1.0:
-                    box[1] = max(int(box[1]) - 2, 0)
-                    box[3] = min(int(box[3]) + 2, 255)
 
-                all_boxes.append(box)
+                all_boxes.append(self._expand_box(box))
                 all_points.append(pt)
 
                 assert type_map[pt[0, 1], pt[0, 0]] > 0
@@ -135,8 +138,8 @@ class DataFolder(Dataset):
                     random.choice(
                         torch.argwhere(mask_single_cell)
                     )[None, [1, 0]].float())
-                prompt_boxes.append(
-                    masks_to_boxes(mask_single_cell.unsqueeze(0)))
+                box = masks_to_boxes(mask_single_cell.unsqueeze(0))
+                prompt_boxes.append(self._expand_box(box))
 
             prompt_points = torch.stack(prompt_points, dim=0)
             prompt_boxes = torch.stack(prompt_boxes, dim=0)

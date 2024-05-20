b1db8d457da84e40c5f7f95fcc384e57da2a538e
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-05-20 22:58:58 +0200

    rollback

--- segmentor/utils.py
+++ segmentor/utils.py
@@ -28,7 +28,7 @@ def train_collate_fn(batch):
         all_points.append(x[6])
         cell_nums.append(len(x[2]))
 
-        print([p.shape for p in prompt_boxes])
+        print([p.shape for p in prompt_points])
 
     return (torch.stack(images), torch.cat(masks), torch.cat(prompt_points), torch.cat(prompt_boxes), torch.cat(prompt_labels),
             all_points, all_points_types, torch.as_tensor(cell_nums))

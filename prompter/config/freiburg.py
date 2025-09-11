5a65599e4df3e989f656cfe5e7d30a5bc88a32eb
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-09-11 23:41:56 +0200

    add config

--- prompter/config/freiburg.py
+++ prompter/config/freiburg.py
@@ -34,6 +34,7 @@ data = dict(
         dict(type='ShiftScaleRotate', shift_limit=0.3, scale_limit=0.1, rotate_limit=0, border_mode=0, value=0, p=0.5),
         dict(type='PadIfNeeded', min_height=None, min_width=None, pad_height_divisor=prompter["space"],
              pad_width_divisor=prompter["space"], position="top_left", p=1),
+        dict(type='Resize', height=256, width=256, p=1),
         dict(type='Normalize'),
     ]),
     val=dict(transform=[

ff8312b72fa5e7b18245538f8dc1ecc3b8643d92
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-09-11 21:20:45 +0200

    add config

--- prompter/dataset.py
+++ prompter/dataset.py
@@ -65,7 +65,7 @@ class DataFolder(Dataset):
         elif self.dataset == 'cpm17':
             mask = scipy.io.loadmat(f'../segmentor/{img_path[:-4].replace("Images", "Labels")}.mat')['inst_map']
         else:
-            mask = np.load(f'../segmentor/{img_path.replace("Images", "Masks")[:-4]}.npy', allow_pickle=True)[()][
+            mask = np.load(f'datasets/{self.dataset}/{img_path[:-4]}_instances.npy', allow_pickle=True)[()][
                         'inst_map']
         mask = (mask > 0).astype(float)
 

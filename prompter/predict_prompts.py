f1a0bd0a8dab6af4fba32fa3591db7d3a5ea118c
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-09-22 16:48:14 +0200

    segmentor

--- prompter/predict_prompts.py
+++ prompter/predict_prompts.py
@@ -29,8 +29,9 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
 transform = A.Compose([
-    A.PadIfNeeded(min_height=None, min_width=None, pad_height_divisor=cfg.prompter.space,
-                  pad_width_divisor=cfg.prompter.space, position="top_left", p=1),
+    #A.PadIfNeeded(min_height=None, min_width=None, pad_height_divisor=cfg.prompter.space,
+    #              pad_width_divisor=cfg.prompter.space, position="top_left", p=1),
+    A.Resize(512, 512),
     A.Normalize(),
     ToTensorV2()
 ], p=1)

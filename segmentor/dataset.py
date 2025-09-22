0fe3ad0436739787109e870abaecafd4582f5023
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-09-22 22:16:07 +0200

    segmentor

--- segmentor/dataset.py
+++ segmentor/dataset.py
@@ -117,7 +117,7 @@ class DataFolder(Dataset):
                 #print(mask_single_cell)
                 points =  torch.argwhere(mask_single_cell)
                 for p in points:
-                    print(type_map[p])
+                    print(p, type_map[p])
                 print('!---!')
 
                 pt = random.choice(

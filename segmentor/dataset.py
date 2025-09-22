2981319f0a4a3e2492dc2d9aef260ac7a09e2f6a
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-09-22 22:24:20 +0200

    segmentor

--- segmentor/dataset.py
+++ segmentor/dataset.py
@@ -66,6 +66,7 @@ class DataFolder(Dataset):
             img_name = Path(img_path).name
             inst_map = np.load(f'datasets/{self.dataset}/{img_name[:-4]}_instances.npy', allow_pickle=True).astype(np.int32)
             type_map = np.load(f'datasets/{self.dataset}/{img_name[:-4]}_types.npy', allow_pickle=True).astype(np.int32)
+            print(np.unique(type_map))
             print(img_name[:-4])
             mask = np.concatenate([inst_map[..., None], type_map[..., None]], axis=-1)
             #mask = (mask > 0).astype(float)

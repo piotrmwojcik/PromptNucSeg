1225e10b6beadb6daf760953e08a42b08e759c47
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-09-23 01:30:24 +0200

    segmentor

--- segmentor/dataset.py
+++ segmentor/dataset.py
@@ -66,10 +66,6 @@ class DataFolder(Dataset):
             img_name = Path(img_path).name
             inst_map = np.load(f'datasets/{self.dataset}/{img_name[:-4]}_instances.npy', allow_pickle=True).astype(np.int32)
             type_map = np.load(f'datasets/{self.dataset}/{img_name[:-4]}_types.npy', allow_pickle=True).astype(np.int32)
-            print(np.unique(type_map))
-            print(img_name[:-4])
-            all_equal = torch.equal(torch.tensor(inst_map) > 0, torch.tensor(type_map) > 0)
-            print('all_equal ', all_equal)
             mask = np.concatenate([inst_map[..., None], type_map[..., None]], axis=-1)
             #mask = (mask > 0).astype(float)
         else:

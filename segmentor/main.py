67a30cd6dc17b29296e4559b034bc7dfbbc042ba
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-09-23 11:38:25 +0200

    eval

--- segmentor/main.py
+++ segmentor/main.py
@@ -344,9 +344,6 @@ def evaluate(
         images = images.to(device)
         inst_maps = inst_maps.numpy()
 
-        print('!!! ')
-        print(images.shape)
-        print(inst_maps.shape)
 
         type_maps = F.one_hot(
             type_maps.type(torch.int64),
